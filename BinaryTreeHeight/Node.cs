8ef16c991dc1ef8bb39b193834da3d5a71624a11
DC <DC@DESKTOP-69F643K>
2020-12-13 20:38:02 +0100

    Add project files.

---
+++ BinaryTreeHeight/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeHeight
+{
+    class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+
+        public Node()
+        {
+            Left = null;
+            Right = null;
+        }
+
+        public static Node createNode(int number)
+        {
+            return new Node(number);
+        }
+    }
+}
---
+++ BinaryTreeHeight/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BinaryTreeHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node Head = Node.createNode(70);
+
+            Node Node1 = Node.createNode(50);
+            Node Node2 = Node.createNode(90);
+            Node Node3 = Node.createNode(55);
+
+            // Level 1
+            Head.Left = Node1;
+            Head.Right = Node2;
+
+            // Level 2
+            Node1.Left = Node.createNode(35);
+            Node1.Right = Node.createNode(25);
+            Node2.Left = Node.createNode(75);
+            Node1.Right = Node3;
+
+            // Level 3
+            Node3.Left = Node.createNode(10);
+            Node3.Right = Node.createNode(30);
+
+            Console.WriteLine($"Height of a Binary Tree is :{Height(Head)}");
+        }
+
+        public static int Height(Node root)
+        {
+            if (null == root)
+                return 0;
+            int hLeftSub = Height(root.Left);
+            int hRightSub = Height(root.Right);
+            return Math.Max(hLeftSub, hRightSub) + 1;
+        }
+    }
+}
